fae010fc912e65aa325316c3df38462a64d17d72
Hrishikesh <hrishikeshc041@gmail.com>
2026-03-01 12:11:28 +0530

    fix: update Prisma client imports and ensure proper module exports

--- server/dist/configs/prisma.js
+++ server/dist/configs/prisma.js
@@ -1,13 +1,8 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
+import { PrismaClient } from "../generated/prisma/client.js";
+const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaPg({ connectionString });
-
-const prisma = new PrismaClient({
-  adapter,
+export const prisma = new PrismaClient({
+    adapter,
 });
-
-export { prisma };
